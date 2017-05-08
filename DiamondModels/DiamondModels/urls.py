e3250cb2c1451d84705d3551bc145d89b764101a
FR98 <franciscorosal98@gmail.com>
2017-05-07 23:03:54 -0600

    Urls

--- DiamondModels/DiamondModels/urls.py
+++ DiamondModels/DiamondModels/urls.py
@@ -15,29 +15,29 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-#from modelos import views as modelos_views
-#from agencias import views as agencias_views
 #from marcas import views as marcas_views
+#from agencias import views as agencias_views
+#from modelos import views as modelos_views
 #from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    #url(r'^$', )
+    #url(r'^$', name= "index")
 
     #url(r'^login/$', auth_views.login, name='login'),
+
+    #url(r'^marca/$', 
+    #    marcas_views.mi_marca, 
+    #    name = "mi_marca"),
+
+    #url(r'^agencia/$', 
+    #    agencias_views.mi_agencia, 
+    #    name = "mi_agencia"),
 
     #url(r'^modelos/$', 
     #    modelos_views.lista_modelos, 
     #    name = "lista_modelos"),
-    
-    #url(r'^agencias/$', 
-    #    agencias_views.lista_agencias, 
-    #    name = "lista_agencias"),
-
-    #url(r'^marcas/$', 
-    #    marcas_views.lista_marcas, 
-    #    name = "lista_marcas"),
 
     #url(r'^agencias/(?P<agencia_pk>[0-9]+)', 
     #    agencias_views.detalle_agencia, 
