66967a2a73e82a9f43d221bbac8383024850c5d7
KarenCaballeros <anakarencaballeros@gmail.com>
2017-05-15 08:34:19 -0600

    aaaaa

---
+++ DiamondModels/marcas/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from marcas.models import Marca
+
+
+class MarcaRegistroForm(forms.ModelForm):
+	class Meta:
+		model = Marca
+		fields = [
+			'nombre', 
+			'telefono',
+			'correo']
+
+class MarcaLoginForm(forms.ModelForm):
+	class Meta:
+		model = Marca
+		fields = [
+			'correo',
+			'contraseña']
