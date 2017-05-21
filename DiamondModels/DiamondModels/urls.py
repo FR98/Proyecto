1605c200eddec72b446bee15ad5bf5225766e6a4
FR98 <franciscorosal98@gmail.com>
2017-05-20 22:06:16 -0600

    css home

--- DiamondModels/DiamondModels/urls.py
+++ DiamondModels/DiamondModels/urls.py
@@ -15,36 +15,35 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 #from marcas import views as marcas_views
 #from agencias import views as agencias_views
 #from modelos import views as modelos_views
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^$', auth_views.login, name='home'),
 
-    url(r'^marcas/(?P<marca_pk>[0-9]+)', 
-        marcas_views.detalle_marca, 
-        name = "detalle_marca"),
+    #url(r'^marcas/(?P<marca_pk>[0-9]+)', 
+    #    marcas_views.detalle_marca, 
+    #    name = "detalle_marca"),
 
 
-    url(r'^agencias/(?P<agencia_pk>[0-9]+)', 
-        agencias_views.detalle_agencia, 
-        name = "detalle_agencia"),
+    #url(r'^agencias/(?P<agencia_pk>[0-9]+)', 
+    #    agencias_views.detalle_agencia, 
+    #    name = "detalle_agencia"),
 
-    url(r'^marcas/$', 
-        marcas_views.mi_marca, 
-        name = "mi_marca"),
+    #url(r'^marcas/$', 
+    #    marcas_views.mi_marca, 
+    #    name = "mi_marca"),
 
-    url(r'^agencias/$', 
-        agencias_views.mi_agencia, 
-        name = "mi_agencia"),
+    #url(r'^agencias/$', 
+    #    agencias_views.mi_agencia, 
+    #    name = "mi_agencia"),
 
-    url(r'^modelos/$', 
-        modelos_views.lista_modelos, 
-        name = "lista_modelos"),
-
-
+    #url(r'^modelos/$', 
+    #    modelos_views.lista_modelos, 
+    #    name = "lista_modelos"),
 ]
